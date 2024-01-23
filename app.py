7b70fc7b5795b96443129e4917175a5a78289a8e
Michael Okoro <michaelokoro1@gmail.com>
2024-01-23 14:48:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ except Exception as e:
     st.error(f"Error loading the model: {str(e)}")
 
 # Define the class labels
-class_labels = ['Adenocarcinoma', 'Largecellcarcinoma', 'Squamouscellcarcinoma', 'Normal']
+class_labels = ['Adenocarcinoma', 'Large cell carcinoma', 'Squamous cell carcinoma', 'Normal, No cancer']
 
 # Streamlit app
 st.title("Lung Cancer Detection App")

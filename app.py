9b353ab25b3e9594e3bc5090442895f85c0fc64e
Michael Okoro <michaelokoro1@gmail.com>
2024-01-24 13:56:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the saved model
+try:
+    model = tf.keras.models.load_model('cancer_detection_model.h5')
+except Exception as e:
+    st.error(f"Error loading the model: {str(e)}")
+
+# Define the relatable class labels
+class_labels = ['adenocarcinoma', 'large.cell.carcinoma', 'squamous.cell.carcinoma', 'normal']
+
+# Streamlit app
+st.title("Lung Cancer Detection App")
+
+# Sidebar navigation
+page = st.sidebar.selectbox("Choose a page", ["Prediction", "Performance Analysis"])
+
+# Upload image through Streamlit
+uploaded_file = st.file_uploader("Choose a test image...", type=["jpg", "jpeg", "png"])
+
 # Model performance analysis
 true_classes = []
 predicted_classes = []
 
 if page == "Prediction":
-     if uploaded_file is not None:
+    if uploaded_file is not None:
         # Load and preprocess the test image
         test_image = image.load_img(uploaded_file, target_size=(150, 150))
         test_image = image.img_to_array(test_image)
@@ -27,7 +53,6 @@ if page == "Prediction":
 
         # Print the classification label with probability
         st.success(f'Predicted Class: {predicted_class_label} with {predicted_class_probability:.2f}% probability')
-
 
 elif page == "Performance Analysis":
     uploaded_files = st.file_uploader("Choose test images (or finish to analyze):", type=["jpg", "jpeg", "png"], accept_multiple_files=True)

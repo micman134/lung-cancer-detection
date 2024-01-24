a433f65f7d3ba0619df4dce1245b915a96e556a4
Michael Okoro <michaelokoro1@gmail.com>
2024-01-24 14:50:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -25,31 +24,31 @@ if page == "Prediction":
     # Upload image through Streamlit
     uploaded_file = st.file_uploader("Choose a test image...", type=["jpg", "jpeg", "png"])
     
-   # Model performance analysis
-   if uploaded_file is not None:
-     # Load and preprocess the test image
-     test_image = image.load_img(uploaded_file, target_size=(150, 150))
-     test_image = image.img_to_array(test_image)
-     test_image = np.expand_dims(test_image, axis=0)
-     test_image = test_image / 255.0  # Normalize
+    # Model performance analysis
+    if uploaded_file is not None:
+        # Load and preprocess the test image
+        test_image = image.load_img(uploaded_file, target_size=(150, 150))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        test_image = test_image / 255.0  # Normalize
     
-    # Perform inference for prediction
-    predictions = model.predict(test_image)
-    predicted_class_index = np.argmax(predictions)
-    predicted_class_label = class_labels[predicted_class_index]
-    predicted_class_probability = predictions[0][predicted_class_index] * 100
+        # Perform inference for prediction
+        predictions = model.predict(test_image)
+        predicted_class_index = np.argmax(predictions)
+        predicted_class_label = class_labels[predicted_class_index]
+        predicted_class_probability = predictions[0][predicted_class_index] * 100
 
-    # Display the uploaded image
-     st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+        # Display the uploaded image
+        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
 
-    # Display probability scores for each class
-    st.write("Class Probabilities:")
-    for label, probability in zip(class_labels, predictions[0] * 100):
-        st.write(f"{label}: {probability:.2f}%")
+        # Display probability scores for each class
+        st.write("Class Probabilities:")
+        for label, probability in zip(class_labels, predictions[0] * 100):
+            st.write(f"{label}: {probability:.2f}%")
 
-    # Print the classification label with probability
-    st.success(f'Predicted Class: {predicted_class_label} with {predicted_class_probability:.2f}% probability')
+        # Print the classification label with probability
+        st.success(f'Predicted Class: {predicted_class_label} with {predicted_class_probability:.2f}% probability')
 
 elif page == "Performance Analysis":
     # Perform inference for performance analysis
-    st.image('confusion_matrix.png', caption="Uploaded Image", use_column_width=True)
+    st.image('confusion_matrix.png', caption="Confusion Matrix", use_column_width=True)

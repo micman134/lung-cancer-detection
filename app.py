41b6792dbe6117b9fe38412a1a1121b7d2c7b299
Michael Okoro <michaelokoro1@gmail.com>
2024-01-24 13:54:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,9 @@
-import streamlit as st
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
-from sklearn.metrics import classification_report, confusion_matrix
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# Load the saved model
-try:
-    model = tf.keras.models.load_model('cancer_detection_model.h5')
-except Exception as e:
-    st.error(f"Error loading the model: {str(e)}")
-
-# Define the relatable class labels
-class_labels = ['adenocarcinoma', 'large.cell.carcinoma', 'squamous.cell.carcinoma', 'normal']
-
-# Streamlit app
-st.title("Lung Cancer Detection App")
-
-# Sidebar navigation
-page = st.sidebar.selectbox("Choose a page", ["Prediction", "Performance Analysis"])
-
-# Upload image through Streamlit
-uploaded_file = st.file_uploader("Choose a test image...", type=["jpg", "jpeg", "png"])
-
 # Model performance analysis
 true_classes = []
 predicted_classes = []
 
 if page == "Prediction":
-    if uploaded_file is not None:
+     if uploaded_file is not None:
         # Load and preprocess the test image
         test_image = image.load_img(uploaded_file, target_size=(150, 150))
         test_image = image.img_to_array(test_image)
@@ -54,12 +28,11 @@ if page == "Prediction":
         # Print the classification label with probability
         st.success(f'Predicted Class: {predicted_class_label} with {predicted_class_probability:.2f}% probability')
 
-elif page == "Performance Analysis":
-    while True:
-        uploaded_file = st.file_uploader("Choose a test image (or finish to analyze):", type=["jpg", "jpeg", "png"])
-        if uploaded_file is None:
-            break
 
+elif page == "Performance Analysis":
+    uploaded_files = st.file_uploader("Choose test images (or finish to analyze):", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+
+    for uploaded_file in uploaded_files:
         # Load and preprocess the test image
         test_image = image.load_img(uploaded_file, target_size=(150, 150))
         test_image = image.img_to_array(test_image)
